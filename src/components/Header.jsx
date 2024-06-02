1b28b8359103d3407d2b90e80e609f165564c5e7
Dawid <dawid.marchewczyk@gmail.com>
2024-06-02 13:22:49 +0200

    create modal with portal and manage state via reducer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import logoImg from "../assets/logo.jpg";
+import Button from "./UI/Button";
+import CartContext from "../store/CartContext";
 
 export default function Header() {
+  const { items } = useContext(CartContext);
+  const totalCartItems = items.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  }, 0);
   return (
     <header id="main-header">
       <div id="title">
@@ -9,7 +15,7 @@ export default function Header() {
         <h1>ReactFood</h1>
       </div>
       <nav>
-        <button>Cart (0)</button>
+        <Button textOnly>Cart ({totalCartItems})</Button>
       </nav>
     </header>
   );
---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+export default function Modal({ children, open, className = "" }) {
+  const dialog = useRef();
+
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.closeModal();
+    }
+  }, [open]);
+  return createPortal(
+    <dialog ref={dialog} className={`modal ${className}`}>
+      {children}
+    </dialog>,
+    document.getElementById("modal")
+  );
+}
